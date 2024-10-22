ac3c4e357b7950eea97a6bf238d34cf3908197fa
Amanturov7 <edi.amanturov@gmail.com>
2024-10-22 09:51:22 +0600

    init

---
+++ src/test/java/kg/amanturov/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.amanturov.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
